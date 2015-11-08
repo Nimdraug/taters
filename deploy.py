be621519bd3a3502089e86f1a7f48400397bf247
Martin Hulten-Ashauer <martin@mortos.org>
2015-11-08 18:39:23 +1100

    initial mockup test

---
+++ deploy.py
@@ -0,0 +1,19 @@
+
+
+def a( files ):
+    for f in files:
+        print f.read()
+
+def b( files ):
+    for f in files:
+        if f.name.endswith( '.txt' ):
+            pipe = object()
+            # todo: run in thread to allow concurrency
+            pipe.write( f.read().upper() )
+            yield pipe
+
+def c():
+    for f in os.listdir():
+        yield open( f )
+
+a( b( c() ) )
