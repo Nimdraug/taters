b55662447c5d3f501bcd83859a00c4f26caef55b
Martin Hulten-Ashauer <martin@designondemand.com.au>
2016-06-15 17:13:11 +1000

    stub setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python2
+from setuptools import setup
+
+setup(
+    name = 'deploy',
+    version = '0.1-dev',
+    
+    maintainer = u'Martin Hult\xe9n-Ashauer',
+    maintainer_email = 'martin@designondemand.com.au',
+    description = 'A fully modular build and deployment framework',
+    
+    py_modules = [
+        'deploy'
+    ]
+
+)
